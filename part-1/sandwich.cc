dd9fb175bf66448f9df8310087480908857f088a
Saldana <bsal@cs300u-2a.acad.fullerton.edu>
2023-11-01 14:26:58 -0700

    Added header and linter errors fixed

--- part-1/sandwich.cc
+++ part-1/sandwich.cc
@@ -1,4 +1,7 @@
-// TODO: Add the required header
+// Izak Ponce
+// izakponce@csu.fullerton.edu
+// @payingatt007
+// Partners:@gtran65, @espacio-vacio,
 
 #include <iostream>
 #include <string>
@@ -7,17 +10,17 @@
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
 
-  // TODO: Validate that the number of arguments is correct.
-  // If not, print an error message and return a non-zero value.
+  // XTODO: Validate that the number of arguments is correct.
+  // XIf not, print an error message and return a non-zero value.
 
-  // TODO: Declare three std::string variables to hold the
-  // protein, bread, and condiment input.
-  // Initialize each variable with an element of the arguments vector
-  // declared above.
+  // XTODO: Declare three std::string variables to hold the
+  // Xprotein, bread, and condiment input.
+  // XInitialize each variable with an element of the arguments vector
+  // Xdeclared above.
 
-  // TODO: Use cout to print output following the pattern
-  // Your order: A *PROTEIN* sandwich on *BREAD* with *CONDIMENT*.
-  // on its own line.
+  // XTODO: Use cout to print output following the pattern
+  // XYour order: A *PROTEIN* sandwich on *BREAD* with *CONDIMENT*.
+  // Xon its own line.
 
   return 0;
 }
