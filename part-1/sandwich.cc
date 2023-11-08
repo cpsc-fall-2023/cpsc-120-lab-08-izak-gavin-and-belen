e96d0056c0170f8a4b128eae1f43a1d960ec1927
gtran65 <143291696+gtran65@users.noreply.github.com>
2023-11-08 23:19:25 +0000

    Finished up the labs and ensured no linter, format, or test case error.

--- part-1/sandwich.cc
+++ part-1/sandwich.cc
@@ -9,7 +9,16 @@
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
+  if (arguments.size() != 4) {
+    std::cout << "error: you must supply three arguments\n";
 
+    return 1;
+  }
+  std::string protein = arguments[1];
+  std::string bread = arguments[2];
+  std::string condiment = arguments[3];
+  std::cout << "Your order: A " << protein << " sandwich on " << bread
+            << " with " << condiment << "\n.";
   // XTODO: Validate that the number of arguments is correct.
   // XIf not, print an error message and return a non-zero value.
 
--- part-2/average.cc
+++ part-2/average.cc
@@ -9,41 +9,23 @@
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
-  if (arguments.size() <= 1) {
-    std::cerr << "Error: Please provide at least one command line argument."
-              << std::endl;
+
+  if (arguments.size() < 2) {
+    std::cout << "error: you must supply at least one number";
     return 1;
   }
-  // XTODO: Validate that there is at least one command line argument.
-  // XIf not, print an error message and return a non-zero value.
-  double sum = 0.0;
-  // XTODO: Write a for-each loop to sum (add up) all of the command line
-  // Xarguments.
-  // XUse a double or float type so that your program preserves fractional
-  // Xvalues.
-  for (int i = 1; i < arguments.size(); ++i) {
-    try {
-      double value = std::stod(arguments[i]);
-      sum += value;
-    } catch (const std::invalid_argument& e) {
-      std::cerr << "Error: Invalid argument found - " << arguments[i]
-                << std::endl;
-      return 1;
+  double sum{0.0};
+  int count = 0;
+  for (std::string& value : arguments) {
+    if (count == 0) {
+      count = 1;
+      continue;
     }
+    sum += std::stod(value);
   }
-  // XThe loop needs to skip over the command name, which is the first element
-  // Xof the arguments vector.
-  // XEach argument is a std::string. You will need to convert each string into
-  // Xa number with the std::stod or std::stof function.
-
-  // XTODO: After the loop has finished summing the arguments, calculate the
-  // Xaverage of the values. Recall that the average is the total value divided
-  // Xby the number of values.
-  double average = sum / (arguments.size() - 1);
-  // XTODO: Use cout to print out a message of the form
-  // Xaverage = *AVERAGE*
-  std::cout << "average = " << average << std::endl;
-  // Xon its own line.
+  double average{0.0};
+  average = sum / static_cast<double>(arguments.size() - 1);
+  std::cout << "average = " << average;
 
   return 0;
 }
